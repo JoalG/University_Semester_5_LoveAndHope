2aa29342b660e241f866b5d342a04db6f067c2df
JoalG <53712282+JoalG@users.noreply.github.com>
2021-06-05 02:14:46 -0600

    Add Navbar routes

--- loveAndHopeWebApp/src/app/app-routing.module.ts
+++ loveAndHopeWebApp/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { TraysComponent } from './components/trays/trays.component';
+import { InfoComponent } from "./components/info/info.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'home', component:HomeComponent},
+  {path:'trays', component:TraysComponent},
+  {path:'info', component:InfoComponent},
+
+  {path:'**',pathMatch:'full',redirectTo:'home'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
