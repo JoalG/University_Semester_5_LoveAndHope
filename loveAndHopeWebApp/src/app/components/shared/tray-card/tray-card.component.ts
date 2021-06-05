86debbc5e86ce876e0b6bf28a7f0b1ac868b6a5f
JOQUINYD <joaquinenrique2010@gmail.com>
2021-06-05 14:00:55 -0600

    add tray-card, carousel

--- loveAndHopeWebApp/src/app/components/shared/tray-card/tray-card.component.ts
+++ loveAndHopeWebApp/src/app/components/shared/tray-card/tray-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tray-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tray-card.component.css']
 })
 export class TrayCardComponent implements OnInit {
+
+  @Input() tray : any = {};
 
   constructor() { }
 
