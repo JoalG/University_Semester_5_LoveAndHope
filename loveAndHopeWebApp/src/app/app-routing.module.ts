1f6fdc5a9900e5029a36e8d7c5f14d1b83c62566
JoalG <53712282+JoalG@users.noreply.github.com>
2021-06-08 18:34:32 -0600

    Add sign-up component

--- loveAndHopeWebApp/src/app/app-routing.module.ts
+++ loveAndHopeWebApp/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { TraysComponent } from './components/trays/trays.component';
 import { InfoComponent } from "./components/info/info.component";
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
 
   {path:'home', component:HomeComponent},
   {path:'trays', component:TraysComponent},
   {path:'info', component:InfoComponent},
+  {path:'sign-up', component:SignUpComponent},
 
   {path:'**',pathMatch:'full',redirectTo:'home'}
 
