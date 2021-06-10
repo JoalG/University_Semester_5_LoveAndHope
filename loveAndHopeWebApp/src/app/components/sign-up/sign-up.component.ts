bc34a9b87c6c709bedfb25c0af0d39200d863479
JOQUINYD <joaquinenrique2010@gmail.com>
2021-06-10 13:28:14 -0600

    update signUp form

--- loveAndHopeWebApp/src/app/components/sign-up/sign-up.component.ts
+++ loveAndHopeWebApp/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    confirnPassword: ['', [Validators.required]],
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required], Validators.email],
+    phone_number: ['', [Validators.required]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
